3e5abb483837fa8836f2bc09186c66b81698f28d
vishnupriya-whitehatjr <whitehatjr.priya@gmail.com>
2020-07-15 04:50:31 +0530

    Update game.js

--- js/game.js
+++ js/game.js
@@ -66,8 +66,8 @@ class Game{
                     
                          textSize(25);
                          fill("white");
-                         text("Score :" +allPlayers.player1.score,50,50);
-                        text("Score :" + allPlayers.player2.score, 50, 100);
+                         text("Player 1 :" +allPlayers.player1.score,50,50);
+                        text("Player 2 :" + allPlayers.player2.score, 50, 100);
                         
                  }
                 
