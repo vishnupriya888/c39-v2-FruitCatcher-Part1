009d44809c425c06f38e7f73b7571a9f96103d80
vishnupriya-whitehatjr <whitehatjr.priya@gmail.com>
2020-07-26 12:17:49 +0530

    commit

--- js/game.js
+++ js/game.js
@@ -64,11 +64,7 @@ class Game{
                          
                      }
                     
-                         textSize(25);
-                         fill("white");
-                         text("Player 1 :" +allPlayers.player1.score,50,50);
-                        text("Player 2 :" + allPlayers.player2.score, 50, 100);
-                 
+                        
                  }
                 
                 
@@ -107,8 +103,7 @@ class Game{
                       for (var i = 0; i < fruitGroup.length; i++) {
                           if (fruitGroup.get(i).isTouching(players)) {
                               fruitGroup.get(i).destroy();
-                              player.score =player.score+1;
-                              player.update();
+                            
                               
                           }
                           
--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ var back_img;
 var gameState =0;
 var playerCount = 0;
 var allPlayers;
-var score =0;
+
 var player, form,game;
 var player1,player2;
 var players;
@@ -11,8 +11,7 @@ var fruits;
 var fruitGroup;
 var fruit1_img, fruit2_img, fruit3_img, fruit4_img, fruit5_img;
 var player_img;
-var player1score =0;
-var player2score =0;
+
 
 function preload(){
   back_img = loadImage("images/jungle.jpg");
