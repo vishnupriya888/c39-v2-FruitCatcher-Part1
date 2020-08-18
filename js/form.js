2b40dcb8f9b68079475664aa6cfe818d1c3e97e5
vishnupriya-whitehatjr <whitehatjr.priya@gmail.com>
2020-08-19 04:44:40 +0530

    Update form.js

--- js/form.js
+++ js/form.js
@@ -47,6 +47,8 @@ class Form{
         this.reset.mousePressed(() => {
             player.updateCount(0);
             game.update(0);
+            database.ref('/').update({players:null});
+
         });
 
     }
